039be52504c8986aa62893e221c9f06bbbc4fa2a
Yordan Madzhunkov <yordan.madzhunkov@metacraft-labs.com>
2024-06-03 16:25:42 +0300

    Errors(kotenca) interface signatures match

--- crates/core/tests/hello_component.rs
+++ crates/core/tests/hello_component.rs
@@ -1,12 +1,12 @@
-
-//use crate::hello_component::hello::test::test::gggg2;
-//use crate::hello_component::gggg2;
-
 pub mod hello {
     wasmtime::component::bindgen!("hello" in "tests/core-wasi-test/wit");
 
     use spin_core::HostComponent;
+    use anyhow::anyhow;
     use test::test::gggg2;
+    use test::test::kotenca;
+    use test::test::kotenca::{ErrorCode};
+
 
     #[derive(Clone)]
     pub struct HelloHostComponent;
@@ -33,5 +33,33 @@ pub mod hello {
             Ok(format!("Hello bace {x}!"))
         }
     }
+
+    impl kotenca::HostError for HelloHostImpl {
+        fn new(&mut self, code: kotenca::ErrorCode, data: String) -> Result<wasmtime::component::Resource<kotenca::Error>, anyhow::Error> {
+            //Ok(kotenca::Error(code, data))
+            Err(anyhow!("Not implemented"))
+        }
+
+        fn drop(&mut self, val: wasmtime::component::Resource<kotenca::Error>) -> Result<(), anyhow::Error>{
+            Ok(())
+        }
+
+        fn code(&mut self, val: wasmtime::component::Resource<kotenca::Error>) -> Result<ErrorCode, anyhow::Error> {
+            println!("{:?}", &val);
+            //Ok(val.code)
+            Err(anyhow!("Not implemented"))
+        }
+
+        fn data(&mut self, val: wasmtime::component::Resource<kotenca::Error>) -> Result<String, anyhow::Error> {
+            println!("{:?}", &val);
+
+            //Ok(val.data)
+            Err(anyhow!("Not implemented"))
+        }
+    
+    }
+    
+    impl kotenca::Host for HelloHostImpl {}
+
 
 }
