648a3adfa10087c61f1e8a86da607f5673c72963
Yordan Madzhunkov <yordan.madzhunkov@metacraft-labs.com>
2024-07-08 12:14:48 +0300

    Add forgoten file

---
+++ crates/ml/src/imagenet_download.rs
@@ -0,0 +1,74 @@
+use curl::easy::Easy;
+use std::fs;
+use std::path::PathBuf;
+use std::io::Write;
+use hex_literal::hex;
+use sha1::{Sha1, Digest};
+
+pub fn try_download(url: &str, filename: &PathBuf) -> Result<(), anyhow::Error> {
+    let mut easy = Easy::new();
+    easy.url(url)
+        .map_err(|e| anyhow::anyhow!("Error {} when downloading {}", e.to_string(), url))?;
+
+    let mut dst = Vec::new();
+    {
+        let mut transfer = easy.transfer();
+        transfer
+            .write_function(|data| {
+                dst.extend_from_slice(data);
+                Ok(data.len())
+            })
+            .unwrap();
+        transfer
+            .perform()
+            .map_err(|e| anyhow::anyhow!("Error {} when downloading {}", e.to_string(), url))?;
+    }
+    {
+        let mut file = std::fs::File::create(filename)?;
+        file.write_all(dst.as_slice())?;
+    }
+    Ok(())
+}
+
+pub struct OpenvinoModel {
+    pub xml: Vec<u8>,
+    pub weights: Vec<u8>
+}
+
+pub fn check_file_hash(file_data: &Vec<u8>, expected_hash: &[u8; 20]) -> std::io::Result<()>{
+        let mut hasher = Sha1::new();
+        hasher.update(file_data.clone());
+        let sha1_hash = hasher.finalize();
+        let check = sha1_hash == (*expected_hash).into();
+        if check {
+            Ok(())
+        } else {
+            let message = format!("Expected sha1 hash = {:x}", sha1_hash);
+            let e = std::io::Error::new(std::io::ErrorKind::InvalidData, message);
+            Err(e)
+        }
+}
+
+pub fn imagenet_check_models(base_path: &PathBuf)-> std::io::Result<OpenvinoModel> {
+    let imagenet_path = base_path.join("imagenet");
+    let model_xml = fs::read(&imagenet_path.join("model.xml"))?;
+    check_file_hash(&model_xml, &hex!("380a4621bf51ae357cb0eaafab203f214dbb036c"))?;
+    let model_weights = fs::read(&imagenet_path.join("model.bin"))?;
+    check_file_hash(&model_weights, &hex!("a50b3bbd47369e306002193fd18847a186c0bcf4"))?;
+    Ok(OpenvinoModel{
+        xml: model_xml,
+        weights: model_weights,
+    })
+}
+
+pub fn imagenet_download(base_path: &PathBuf) -> Result<(), anyhow::Error>  {
+    let base_url = "https://raw.githubusercontent.com/blocksense-network/imagenet_openvino/db44329b8e2b3398c9cc34dd56d94f3ce6fd6e21/";
+    let imagenet_path = base_path.join("imagenet");
+    fs::create_dir_all(imagenet_path.clone()).unwrap();
+    let files = ["model.xml", "model.bin",];
+    for file in files {
+        try_download(&(base_url.to_owned() + file), &imagenet_path.join(file))?;
+    }
+    Ok(())
+}
+    
